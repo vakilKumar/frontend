c4ea3ddd69342c69a64a5236b7510e1ec4665d1a
vakilKumar <vakilraj26031997@gmail.com>
2024-08-04 21:19:28 +0530

    implement home page desing

---
+++ src/pages/home/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ProductComponet from "../product";
+import FilterComponet from "../filter";
+import "./styles.css";
+import { shallowEqual, useSelector } from "react-redux";
+import { dummyProfileSelector } from "../../store/selectors";
+
+function Home() {
+  const cartData = useSelector(
+    dummyProfileSelector.getDummyApiListData(),
+    shallowEqual
+  );
+  return (
+    <div>
+      <div>
+        <p>{"HomeOnline Fashion Store"}</p>
+      </div>
+      <div>
+        <p>Online Fashion Store - {cartData?.length}items</p>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <div className="filteMainIcon">
+          <FilterComponet />
+        </div>
+        <div>
+          <ProductComponet />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
